6e0165f6bc85684df2b94c48b8f86aa36ef9df99
GabrielSattler <ga.sattler98@gmail.com>
2018-06-06 19:30:18 -0300

    Ejercicio 1 hecho

--- ejercicio1/js/script.js
+++ ejercicio1/js/script.js
@@ -4,11 +4,18 @@ var Table = {
     setData:function(Data){
         this.data = Data;
     },
+
+    clear:function(){
+        $("#table-body").html("");
+    },
+
     listAll:function(){
+        this.clear();
         for(var i = 0; i < this.data.length; i++){
             $("#table-body").append(Table.assamble(this.data[i]));
         }
     },
+
     assamble:function(Data){
         return '<div class="row">' +
                 '<span class="col-2">' + '</span>' +
@@ -20,4 +27,93 @@ var Table = {
                 '<span class="description col-12">' + "" + '</span>' +
             '</div>';
     },
+
+    filtroBrand:function(){
+        Table.clear();
+        var input = document.getElementById("BrandInput").value;
+        if(input == ""){
+            this.listAll();
+        }
+        for(var i = 0; i < this.data.length; i++){
+            if(this.data[i].brand == input){
+                $("#table-body").append('<div class="row">' +
+                            '<span class="col-2">' + '</span>' +
+                            '<span class="col-2">' + this.data[i].brand + '</span>' +
+                            '<span class="col-2">' + this.data[i].location + '</span>' +
+                            '<span class="col-2">' + this.data[i].lunch + '</span>' +
+                            '<span class="col-2">' + this.data[i].price + '</span>' +
+                            '<span class="col-2">' + '</span>' +
+                            '<span class="description col-12">' + "" + '</span>' +
+                        '</div>');
+            }
+        }
+    },
+
+    filtroLocation:function(){
+        Table.clear();
+        var input = document.getElementById("LocationInput").value;
+        if(input == ""){
+            this.listAll();
+        }
+        for(var i = 0; i < this.data.length; i++){
+            if(this.data[i].location == input){
+                $("#table-body").append('<div class="row">' +
+                            '<span class="col-2">' + '</span>' +
+                            '<span class="col-2">' + this.data[i].brand + '</span>' +
+                            '<span class="col-2">' + this.data[i].location + '</span>' +
+                            '<span class="col-2">' + this.data[i].lunch + '</span>' +
+                            '<span class="col-2">' + this.data[i].price + '</span>' +
+                            '<span class="col-2">' + '</span>' +
+                            '<span class="description col-12">' + "" + '</span>' +
+                        '</div>');
+            }
+        }
+    },
+
+    filtroLaunch:function(){
+        Table.clear();
+        var input = document.getElementById("LaunchInput").value;
+        if(input == ""){
+            this.listAll();
+        }
+        for(var i = 0; i < this.data.length; i++){
+            if(this.data[i].lunch == input){
+                $("#table-body").append('<div class="row">' +
+                            '<span class="col-2">' + '</span>' +
+                            '<span class="col-2">' + this.data[i].brand + '</span>' +
+                            '<span class="col-2">' + this.data[i].location + '</span>' +
+                            '<span class="col-2">' + this.data[i].lunch + '</span>' +
+                            '<span class="col-2">' + this.data[i].price + '</span>' +
+                            '<span class="col-2">' + '</span>' +
+                            '<span class="description col-12">' + "" + '</span>' +
+                        '</div>');
+            }
+        }
+    },
+
+    filtroPrice:function(){
+        Table.clear();
+        var input = document.getElementById("PriceInput").value;
+        if(input == ""){
+            this.listAll();
+        }
+        for(var i = 0; i < this.data.length; i++){
+            if(this.data[i].price == input){
+                $("#table-body").append('<div class="row">' +
+                            '<span class="col-2">' + '</span>' +
+                            '<span class="col-2">' + this.data[i].brand + '</span>' +
+                            '<span class="col-2">' + this.data[i].location + '</span>' +
+                            '<span class="col-2">' + this.data[i].lunch + '</span>' +
+                            '<span class="col-2">' + this.data[i].price + '</span>' +
+                            '<span class="col-2">' + '</span>' +
+                            '<span class="description col-12">' + "" + '</span>' +
+                        '</div>');
+            }
+        }
+    },
 }
+
+$(function() {
+    Table.setData(elementsList);
+    Table.listAll();
+});
